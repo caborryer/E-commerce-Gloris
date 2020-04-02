ec2b844183f4a8f836537ddc982cee91dfc7407b
caborryer <corraleshyulia@gmail.com>
2020-04-02 13:18:15 -0500

    making secure routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { HomeComponent } from './components/home/home.component';
 import { ChatComponent } from './components/chat/chat.component';
 import { ProductsComponent } from './components/products/products.component';
 import { UserComponent } from './components/user/user.component';
+import { AuthGuard } from './service/auth.guard';
 
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
-  {path: 'messages', component: ChatComponent},
-  {path: 'products', component: ProductsComponent},
-  {path: 'profile', component: UserComponent},
+  {path: 'messages', component: ChatComponent, canActivate: [ AuthGuard]},
+  {path: 'products', component: ProductsComponent, canActivate: [ AuthGuard]},
+  {path: 'profile', component: UserComponent, canActivate: [ AuthGuard]},
   {path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
---
+++ src/app/service/auth.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanActivateChild, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate, CanActivateChild {
+
+  constructor(private auth: AuthService) {
+  }
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean>  {
+    return this.auth.isAuthenticated$;
+  }
+  canActivateChild(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
+  }
+  
+}
